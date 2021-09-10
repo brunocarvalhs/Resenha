a7117c1e413cf56ebbcf72542c58381fc2698563
BrunoCarvalhs <bruno.silva.carvalho@outlook.com.br>
2021-09-10 16:59:35 -0300

    support plataform created

--- test/app/app_module_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:resenha/app/app_module.dart';
-import 'package:flutter_modular_test/flutter_modular_test.dart';
-
-void main() {
-  setUpAll(() {
-    initModule(AppModule());
-  });
-}
